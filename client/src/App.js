59348f94de05b265eefec6171b2f84b74549e545
Meagan Truong <MeaganTruong1@yahoo.com>
2024-01-22 14:55:07 -0600

    working search function / moved

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React, { useState, useEffect } from "react";
-import MainPage from './components/MainPage'
+import MainPage from './components/home/MainPage'
+
+
 function App() {
   const url = "http://localhost:3001"
   const [emails, setEmails] = useState([])
-  const[search,setSearch] = useState('')
   useEffect(() => {
     console.log("Fetching emails...")
     fetch(url + "/emails")
@@ -17,43 +18,38 @@ function App() {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
   
-  const filtered = emails.filter(email => email.name.toUpperCase().includes(search.toUpperCase()))
-  
-  const updateSearch = (e) => {
-    setSearch(e.target.value)
-  }
 
     return (
     <div id="app-container">
-      <div><MainPage/></div>
+      <div><MainPage emails={emails}/></div>
       
       
     </div>
   );
 }
 
-// Configure our Router
-const router = createBrowserRouter([
-  {
-    path: "/",
-    // Props to Home gets passed here
-    element: <Home />
-  },
-  {
-    path: "/Replies",
-    // Props to Replies gets passed here
-    element: <Replies />
-  },
-  {
-    path: "/Statistics",
-    // Props to Statistics gets passed here
-    element: <Statistics />
-  },
-  {
-    path: "/Companies",
-    // Props to Companies gets passed here
-    element: <Companies />
-  },
-]);
+// // Configure our Router
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     // Props to Home gets passed here
+//     element: <Home />
+//   },
+//   {
+//     path: "/Replies",
+//     // Props to Replies gets passed here
+//     element: <Replies />
+//   },
+//   {
+//     path: "/Statistics",
+//     // Props to Statistics gets passed here
+//     element: <Statistics />
+//   },
+//   {
+//     path: "/Companies",
+//     // Props to Companies gets passed here
+//     element: <Companies />
+//   },
+// ]);
 
 export default App;
