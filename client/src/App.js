5c262a6170cc993ecc6050b92a4ac692737814cb
Nicholas <najernigin@gmail.com>
2024-01-22 10:54:53 -0600

    Working react

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const url = "http://localhost:3001"
+  const [emails, setEmails] = useState([])
+  const[search,setSearch] = useState('')
+  useEffect(() => {
+    console.log("Fetching emails...")
+    fetch(url + "/emails")
+      .then((res) => res.json())
+      .then((emails) => {
+        setEmails(emails);
+        console.log("Emails:",emails)
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+  
+  const filtered = emails.filter(email => email.name.toUpperCase().includes(search.toUpperCase()))
+  
+  const updateSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
+    return (
+    <div id="app-container">
+      <div><NavBar/></div>
+      <div><SideBar emails={filtered} search={search} updateSearch={updateSearch}/></div>
+      <div><EmailBody/></div>
     </div>
   );
 }
 
-export default App;
+function NavBar(){
+    return(
+      <nav id = 'navbar'> 
+      <ul>
+        <li><p>Email Tracker</p></li>
+        <li><button>Home</button></li>
+        <li><button>Emails</button></li>
+        <li><button>Statistics</button></li>
+        <li><button>Companies</button></li>
+      </ul>
+      </nav>
+  )}
+
+function SideBar({emails,search,updateSearch}){
+   return(
+    <nav id = 'sidebar'>
+      <ul>
+      <input placeholder='search' id='searchbar' value={search} onChange={updateSearch}></input>
+      {emails && emails.map((email) => {
+        return (
+          <EmailCard
+            name ={email.name}
+            key={email.id}
+            email={email}
+           />
+        );
+      })}
+      </ul>
+    </nav>
+  )}
+  
+function EmailCard({email}){
+    return(
+      <li className = 'email'>
+        <p>{email.name}</p>
+      </li>
+    )
+}
+
+function EmailBody(){
+  return(
+    <div id= 'emailbody'>
+      <p>This is the email body</p>
+      <p>Cash App:$Snackpack818</p>
+    </div>
+  )
+}
+  export default App;
