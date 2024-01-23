b738c6a2f2de7ca649189226d2930b4db0d4a46f
Meagan Truong <MeaganTruong1@yahoo.com>
2024-01-23 10:19:58 -0600

    stats card logic

--- client/src/components/statistics/StatsPage.js
+++ client/src/components/statistics/StatsPage.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StatsNav from './StatsNav'
 function StatsPage(){
+    const url = "http://localhost:3001"
+    const [emails, setEmails] = useState([])
+
+    useEffect(() => {
+        console.log("Fetching emails...")
+        fetch(url + "/emails")
+          .then((res) => res.json())
+          .then((emails) => {
+            setEmails(emails);
+            console.log("Emails:",emails)
+          })
+          .catch((error) => console.error("Error fetching data:", error));
+      }, []);
     return(
         <div>
             <StatsNav/>
