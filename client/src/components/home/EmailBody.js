0f1a04036fd26eb76692e7a21e18fa78cbad598d
Nicholas <najernigin@gmail.com>
2024-01-23 16:40:11 -0600

    Attempt at conditional rendering

--- client/src/components/home/EmailBody.js
+++ client/src/components/home/EmailBody.js
@@ -1,8 +1,9 @@
 import React from 'react'
-function EmailBody(){
+import {showComponent,setShowComponent} from './EmailCard'
+function EmailBody({title}){
     return(
       <div id= 'emailbody'>
-        <div id ='title'></div>
+        <div id ='title'><p>{title}</p></div>
         <div id ='subject'></div>
         <div id ='body'></div>
       </div>
