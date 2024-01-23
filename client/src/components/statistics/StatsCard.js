16b9abf318a57623ef45dedd5daa287f84ac6662
Alex Isaacs <alexisaacs18@gmail.com>
2024-01-23 16:41:11 -0500

    added stats

--- client/src/components/statistics/StatsCard.js
+++ client/src/components/statistics/StatsCard.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-function StatsCard({ name }) {
+function StatsCard({ title, sent, replied, unsubscribed }) {
     return (
         <div className="statscard">
-            <li>{name}</li>
+            <li>{title}</li>
+            <li>{`Reply Rate: ${(replied / sent).toFixed(2)}%`}</li>
+            <li>{`Unsubscribe Rate: ${(unsubscribed / sent).toFixed(2)}%`}</li>
         </div>
     )
 }
