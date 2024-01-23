e42a1834e8a187c6ded750a547580cbea5234032
Meagan Truong <MeaganTruong1@yahoo.com>
2024-01-23 17:18:50 -0600

    fixed toggle feature

--- client/src/components/home/MainPage.js
+++ client/src/components/home/MainPage.js
@@ -5,7 +5,7 @@ import EmailBody from './EmailBody'
 function MainPage() {
   const url = "http://127.0.0.1:5555"
   const [emails, setEmails] = useState([])
-  const [selectedEmail, setSelectedEmail] = useState(null);
+  const [selectedEmail, setSelectedEmail] = useState();
   const [showComponent, setShowComponent] = useState(false);
 
 
@@ -21,8 +21,9 @@ function MainPage() {
 
 
   const toggleComponent = (email) => {
-    setShowComponent(!showComponent)
-    setSelectedEmail(email)
+    // Update the selectedEmail state, always show EmailBody
+    setSelectedEmail(email);
+    setShowComponent(true)
   }
   return (
     <div>
