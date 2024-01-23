f9e7f01a5712fc627a5df93588dc7d765f061ab8
Nicholas <najernigin@gmail.com>
2024-01-22 23:50:20 -0600

    Almost working routes

---
+++ client/src/components/companies/CompaniesNav.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useNavigate} from "react-router-dom";
+
+function CompaniesNav(){
+  const navigate = useNavigate();
+  
+    const navigateToHome = () => {
+      navigate('/')
+    };
+    const navigateToReplies = () => {
+      navigate('ReplyPage')
+    };
+    const navigateToStatistics = () => {
+      navigate('Statistics')
+    };
+    const navigateToCompanies = () => {
+      navigate('Companies')
+    };
+
+    return(
+      <nav className ='navbar'> 
+      <ul>
+        <li><p>Email Tracker</p></li>
+        <li><button onClick={navigateToHome}>Home</button></li>
+        <li><button onClick={navigateToReplies}>Replies</button></li>
+        <li><button onClick={navigateToStatistics}>Statistics</button></li>
+        <li><button onClick={navigateToCompanies}>Companies</button></li>
+      
+      </ul>
+      </nav>
+  )}
+export default CompaniesNav;
---
+++ client/src/components/companies/CompaniesPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import CompaniesNav from './CompaniesNav'
+function CompaniesPage(){
+    return(
+        <div><CompaniesNav/></div>
+    )
+}
+export default CompaniesPage()
