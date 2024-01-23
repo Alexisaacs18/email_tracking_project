0bc26a6e9163e0b7ca2e2718fc07c39bf13dc854
Alex Isaacs <alexisaacs18@gmail.com>
2024-01-23 15:48:38 -0500

    edited react

--- client/src/components/statistics/StatsCard.js
+++ client/src/components/statistics/StatsCard.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-function StatsCard({ email, name }) {
-return(
-    <div className = "statscard">
-        <li>{name}</li>
-    </div>
-)}
+function StatsCard({ name }) {
+    return (
+        <div className="statscard">
+            <li>{name}</li>
+        </div>
+    )
+}
 
 export default StatsCard;
