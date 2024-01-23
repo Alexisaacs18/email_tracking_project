269ba3d5b38847b7a122840e1c23b50b65d87162
Nicholas <najernigin@gmail.com>
2024-01-23 15:13:17 -0600

    Email Body ids

--- client/src/components/home/EmailBody.js
+++ client/src/components/home/EmailBody.js
@@ -2,8 +2,9 @@ import React from 'react'
 function EmailBody(){
     return(
       <div id= 'emailbody'>
-        <p>This is the email body</p>
-        <p>Cash App:$Snackpack818</p>
+        <div id ='title'></div>
+        <div id ='subject'></div>
+        <div id ='body'></div>
       </div>
     )
   }
