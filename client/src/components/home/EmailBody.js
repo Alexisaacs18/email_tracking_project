d5820577ed5d7909b501a6c50bfa1508b84648bc
Alex Isaacs <alexisaacs18@gmail.com>
2024-01-24 07:02:44 -0500

    added buttons

--- client/src/components/home/EmailBody.js
+++ client/src/components/home/EmailBody.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { showComponent, setShowComponent } from './EmailCard'
 function EmailBody({ email }) {
-  if (!email) return null;
+
+  const [sent, setSent] = useState(email.number_sent)
+  const [replies, setReplies] = useState(email.number_replied)
+  const [unsubscribed, setUnsubscribed] = useState(email.number_unsubscribed)
+
+  function handleSent() {
+    setSent(prev => prev + 1)
+  }
+
+  function handleReplies() {
+    setReplies(prev => prev + 1)
+  }
+
+  function handleUnsubscribed() {
+    setUnsubscribed(prev => prev + 1)
+  }
 
 
   return (
@@ -9,6 +24,10 @@ function EmailBody({ email }) {
       <div id='title'><p>Title: {email.email_title}</p></div>
       <div id='subject'><p>Subject: {email.subject}</p></div>
       <div id='body'><p>{email.body}</p></div>
+      <button onClick={handleSent}>Sent: {sent}</button>
+      <button onClick={handleReplies}>Replies: {replies}</button>
+      <button onClick={handleUnsubscribed}>Unsubscribed: {unsubscribed}</button>
+
     </div>
   )
 }
