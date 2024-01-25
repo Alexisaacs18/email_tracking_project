0282add1c54dfcc3c964fd37b3aef711e1c3886a
Alex Isaacs <alexisaacs18@gmail.com>
2024-01-25 09:44:48 -0500

    commit

--- client/src/components/home/EmailCard.js
+++ client/src/components/home/EmailCard.js
@@ -1,15 +1,28 @@
 import React from 'react'
 
-function EmailCard({ email, toggleComponent }) {
+function EmailCard({ email, toggleComponent, deleteTemplate }) {
+
+  const url = "http://127.0.0.1:5555"
 
   const handleClick = () => {
     toggleComponent(email)
   }
+
+  function handleDelete() {
+    deleteTemplate(email.id)
+    fetch(`${url}/emails/${email.id}`, {
+      method: "DELETE",
+    })
+  }
+
   return (
-    <div onClick={handleClick}>
-      <li className='email'>
-        <p>{email.email_title}</p>
-      </li>
+    <div>
+      <div onClick={handleClick}>
+        <li className='email'>
+          <p>{email.email_title}</p>
+        </li>
+      </div>
+      <button onClick={handleDelete}>X</button>
     </div>
   )
 }
